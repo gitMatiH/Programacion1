7c07fa7e0f3ead0a4483e4659af32581bd695a5b
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 14:23:07 -0300

    Update Ej9.py

--- Ej9.py
+++ Ej9.py
@@ -13,10 +13,10 @@ print("Calcula la retención en base a ganancias.")
 
 ganancias = int(input ("ingrese sus ganancias anuales:\n"))
 if ganancias <= 10000:
-    print ("retencion = cero")
+    print ("Retencion = cero")
 elif ganancias >10000 and ganancias <= 15000:   # con or estaría mal, podría pasar algo fuera de rango. Aunque el primero lo filtra (creo)
-    retencion = 2 * ganancias / 100 #retencion = (ganancia-15000)*0.02
-    print ("retencion total = ", retencion)
+    retencion = (ganancias - 10000)*2/100
+    print ("Retencion = ", retencion)
 else:   #usa la condición que queda por descarte, en este caso ganancias > 150000
-    retencion = 300 + 5 * ganancias / 100       # retencion = 300 + (ganancia -15000)*0.05
-    print ("retencion total = ", retencion)
+    retencion = 300 + (ganancias - 15000)*5/100
+    print ("Retencion = ", retencion)
