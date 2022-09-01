b211395497a90accd9deb9e5f3c8abb0b02d1619
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 19:38:05 -0300

    Update Ej14.py

--- Ej14.py
+++ Ej14.py
@@ -2,3 +2,15 @@
 Ingresar un numero de dos cifras, si es mayor que 50 mostrarlo invertido. Sino
 mostrar la cifra que corresponde a las unidades.
 '''
+
+numero = int(input("Ingrese un número de dos cifras"))
+while len(str(numero)) != 2:
+	print("Errror")
+	numero = int(input("Ingrese un número de dos cifras"))
+
+criterio = 50
+if criterio < numero:
+	pass
+elif numero <= criterio:
+	pass
+	
