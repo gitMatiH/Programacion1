d33d69c555a2449b2ef7c1687754d19e7bce4204
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 15:26:37 -0300

    Update Ej11.py

--- Ej11.py
+++ Ej11.py
@@ -8,9 +8,9 @@ $0.40 para la llamada interurbana y $1.05 para la llamada internacional, diseña
 algoritmo que permita calcular el monto a pagar por dicha llamada.
 '''
 
-precio_local		= float(0.25)
-precio_interurbano	= float(0.40)
-precio_internacional	= float(1.05)
+precio_minuto_local		= float(0.25)
+precio_minuto_interurbano	= float(0.40)
+precio_minuto_internacional	= float(1.05)
 
 codigo = int(input("1: Local\n"
 				   "2: Interurbana\n"
