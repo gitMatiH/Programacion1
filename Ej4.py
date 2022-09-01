78d40b39742183024ef48d4cf93607e7db15512d
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 13:18:18 -0300

    Create Ej4.py

---
+++ Ej4.py
@@ -0,0 +1,8 @@
+# Hallar la longitud de la hipotenusa de un triángulo dada la medida de sus catetos
+
+print("El triángulo debe ser rectángulo")
+cateto1 = float(input("ingrese cateto 1"))
+cateto2 = float(input("ingrese cateto 2"))
+
+hipotenusa = cateto1^2+cateto2^2
+print("la hipotenusa es: ", hipotenusa)
