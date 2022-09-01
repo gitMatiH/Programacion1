312d6a7585d31467111bbbafaf6c7b44726de5ee
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 16:24:59 -0300

    Create Ej13.py

---
+++ Ej13.py
@@ -0,0 +1,25 @@
+'''
+El usuario deberá pensar en uno de los siguientes personajes: Lio Messi, Mauricio
+Macri y Mirtha Legrand. El programa mediante algunas preguntas convenientes
+(edad, sexo, ocupación, etc.) deberá mostrar de que personaje se trata. Ejemplo: si
+es hombre y deportista tendrá que decir Lio Messi
+'''
+
+class Personaje:
+	def __init__(self, nombre, edad, ocupacion):
+		self.nombre = nombre
+		self.edad = edad
+		self.ocupacion = ocupacion
+
+nombres = ["Lio Messi", "Mauricio Macri", "Mirtha Legrand"]
+edades = [35, 63, 95]
+ocupaciones ["futbolista", "Político", "Conductor/a de TV"]
+# ver como usar zip
+
+# ver como poner esto en un while con nombramiento dinámico, ej "personaje{}".format(i+1) = Personaje(personajes[0]). Hacer antes input edad
+personaje1 = Personaje(nombres[0], edades[0], ocupaciones[0])
+personaje2 = Personaje(nombres[1], edades[1], ocupaciones[1])
+personaje3 = Personaje(nombres[2], edades[2], ocupaciones[2])
+
+
+print("piense en un personaje...")
