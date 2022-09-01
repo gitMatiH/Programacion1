f2d8170bb348efddf91013a6bad6d72828ee8fa0
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 14:27:19 -0300

    Update and rename Ej10Tupla to Ej10Tupla.py

---
+++ Ej10Tupla.py
@@ -0,0 +1,39 @@
+
+
+# Version con tupla
+## ver de hacer pasaje tupla (horas,minutos) a integer minutos
+#python program to take tuple input in python
+values = input('Ingresar tupla de valores (hora,dia), separado por espacio:')
+#spliting the input values by space
+input_tuple = tuple(int(val) for val in values.split())
+print('tuple:',input_tuple)
+
+#Modificar
+
+'''
+Calcular  el  importe  que  debe  pagar  un  auto  en  un  estacionamiento  
+teniendo  en cuenta como datos las horas y minutos de uso. El valor de la hora 
+es de $45 y si los minutos exceden de 15 se incrementa una hora al importe. 
+El mínimo a cobrar es de una hora.
+'''
+
+valorHora = 45
+minHora = 1
+
+minutos = int(input("ingrese cuantas minutos estuvo estacionado: "))
+
+horas = 0
+
+if minutos < 60:
+    print("Se cobra: ", minHora*valorHora)
+
+while minutos > 60:
+    minutos = minutos - 60
+    horas = horas + 1
+
+# hacer con resto
+
+if minutos > 15:
+    horas = horas + 1
+    importe = horas * valorHora
+    print("importe:", importe)
