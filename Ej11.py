051ac3e01f05197daac9ede8b44445c1f8a6c9a7
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 15:19:44 -0300

    Create Ej11.py

---
+++ Ej11.py
@@ -0,0 +1,10 @@
+'''
+Se ingresa el código de tipo de llamada: 
+1. Local 
+2. Interurbana 
+3. Internacional 
+y la duración en minutos de la misma. Si el minuto cuesta $0.25 para la llamada local,
+$0.40 para la llamada interurbana y $1.05 para la llamada internacional, diseñar un
+algoritmo que permita calcular el monto a pagar por dicha llamada.
+'''
+
