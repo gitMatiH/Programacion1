9994b51b4a2430ae175299ed1e5c86e179020276
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 16:31:42 -0300

    Update Ej13.py

--- Ej13.py
+++ Ej13.py
@@ -15,6 +15,10 @@ nombres = ["Lio Messi", "Mauricio Macri", "Mirtha Legrand"]
 edades = [35, 63, 95]
 ocupaciones ["futbolista", "Político", "Conductor/a de TV"]
 # ver como usar zip
+print("datos para jugar\n"
+	  "nombres: ", nombres,
+	  "edades: ",  edades,
+	  "ocupaciones", ocupaciones,"\n")
 
 # ver como poner esto en un while con nombramiento dinámico, ej "personaje{}".format(i+1) = Personaje(personajes[0]). Hacer antes input edad
 personaje1 = Personaje(nombres[0], edades[0], ocupaciones[0])
@@ -23,3 +27,7 @@ personaje3 = Personaje(nombres[2], edades[2], ocupaciones[2])
 
 
 print("piense en un personaje...")
+
+edad = int(input("¿Qué edad tiene? (copiar y pegar)\n"))
+ocupacion = input("¿Cuál es su ocupación? (copiar y pegar)")
+#combinatoria
