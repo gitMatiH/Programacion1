cf8fb40de958b059dc1892ce6659979d97ed8da6
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 14:18:49 -0300

    Update Ej8.py

--- Ej8.py
+++ Ej8.py
@@ -1,1 +1,16 @@
+'''
+Ingresar dos números, calcular y mostrar el cociente del primero por el segundo,
+siempre que el divisor no sea cero. En este último caso mostrar la leyenda “no se
+puede realizar el cociente”.
+'''
 
+num1 = float(input("Ingrese primer número"))
+num2 = float(input("ingrese segundo número"))
+
+print("Calculo cociente de primero por segundo:\n")
+
+if num2 != float(0):
+	cociente = num1/num2
+	print("El cociente es:", cociente)
+else:
+	print("no se puede realizar el cociente")
