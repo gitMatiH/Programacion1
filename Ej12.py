66e865aa502886910368eac98fe57db11f6159a9
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 16:00:04 -0300

    Update Ej12.py

--- Ej12.py
+++ Ej12.py
@@ -22,4 +22,13 @@ t_llegada = time.time()
 t_transcurrido = t_llegada - t_salida
 print("Tiempo transcurrido: %s horas" % t_transcurrido )
 	
-km_actual = int(input("Ingrese el kilometraje inicial que indica el odómetro"))
+km_actual = int(input("Ingrese el kilometraje que indica el odómetro"))
+
+km_recorridos = km_actual - km_inicial
+
+if km_recorridos <= 100:
+	print("Paciencia, falta mucho")
+elif 100 < km_recorridos and km_recorridos <= 200:
+	print("Parar para desayunar")
+elif 200 < km_recorridos:
+	print("Cargar combustible")
