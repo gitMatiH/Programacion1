de199aaec8dfe411e7a2fc822d6988375f40fcb3
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 13:16:03 -0300

    Create Ej1.py

---
+++ Ej1.py
@@ -0,0 +1,10 @@
+# Calcular el sueldo de una persona, conociendo la cantidad de horas que trabaja en
+# el mes y el valor de la hora.
+
+horas = int(input("ingrese cantidad de horas trabajadas en el mes:\n"))
+
+valorhora = float(input("ingrese el valor de una hora de trabajo\n"))
+
+sueldo = horas*valorhora    #autocastea a float, hace un uptype del int
+
+print("sueldo:", sueldo)
