635f64ba728db7d950230ae742456a52b2fcecb7
Matías Hillmann <106032653+gitMatiH@users.noreply.github.com>
2022-09-01 15:55:33 -0300

    Update Ej12.py

--- Ej12.py
+++ Ej12.py
@@ -7,3 +7,19 @@ Más de 100 Km. y menos de 200:	"Parar para desayunar"
 Más de 200 Km.: “Se recomienda:	"Cargar combustible"
 
 '''
+import time
+
+km_inicial = int(input("Ingrese el kilometraje inicial que indica el odómetro"))
+
+## simula viaje
+i = 0
+t_salida = time.time()
+while time < 4:
+	print("viajando")
+	sleep(1)
+	i = i + 1
+t_llegada = time.time()
+t_transcurrido = t_llegada - t_salida
+print("Tiempo transcurrido: %s horas" % t_transcurrido )
+	
+km_actual = int(input("Ingrese el kilometraje inicial que indica el odómetro"))
