8a868ccf9ca8ef2ab275175f78fbf23e615bd27d
Matías Alexander Hillmann <mailmatiash@gmail.com>
2022-09-16 10:59:25 -0300

    Update Ej12.py

--- TP1/Ej12.py
+++ TP1/Ej12.py
@@ -1,12 +1,5 @@
-'''
-Calcular los kilómetros recorridos por un automóvil conociendo el kilometraje inicial
-y el actual. Mostrar una leyenda según la distancia recorrida:
 
-Para 100 Km. o menos:			"Paciencia, falta mucho"
-Más de 100 Km. y menos de 200:	"Parar para desayunar"
-Más de 200 Km.: “Se recomienda:	"Cargar combustible"
 
-'''
 import time
 
 km_inicial = int(input("Ingrese el kilometraje inicial que indica el odómetro: "))
