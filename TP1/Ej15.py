79756ddf99aebb5f6beeaf0a9a34742b9fbd628f
Matías Alexander Hillmann <mailmatiash@gmail.com>
2022-09-16 11:03:31 -0300

    Update Ej15.py

--- TP1/Ej15.py
+++ TP1/Ej15.py
@@ -1,8 +1,3 @@
-'''
-Ingresar la medida de un ángulo y determinar si es agudo, obtuso, recto, nulo o llano.
-Si el valor ingresado es mayor a 180º mostrar la leyenda "ángulo no válido" e
-ingresar un nuevo valor.
-'''
 
 from decimal import Decimal
 
