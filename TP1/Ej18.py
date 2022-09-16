ad48cbd0ef65a7d5b9866f95a51d98b2f94c68ad
Matías Alexander Hillmann <mailmatiash@gmail.com>
2022-09-16 11:06:09 -0300

    Update Ej18.py

--- TP1/Ej18.py
+++ TP1/Ej18.py
@@ -1,10 +1,3 @@
-'''
-Según las notas de los tres trimestres, sacar el promedio.
-Si el promedio es mayor o igual a seis y la nota del tercer trimestre
-es mayor o igual a seis, está aprobado.
-Si no, si el promedio es mayor a cuatro, va a diciembre.
-Si es menor a cuatro, va a marzo.
-'''
 
 Print("Ingresar las notas de cada trimestre")
 n1 = input("primer trimestre")
