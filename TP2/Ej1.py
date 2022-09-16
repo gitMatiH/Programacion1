94564ef21dc2eed21414c671d63e51c315258b19
Matías Alexander Hillmann <mailmatiash@gmail.com>
2022-09-16 11:06:40 -0300

    Update Ej1.py

--- TP2/Ej1.py
+++ TP2/Ej1.py
@@ -1,8 +1,3 @@
-'''
-Diseñar un algoritmo que permita:
-1) Ingresar números hasta un múltiplo de 3.
-Mostrar el último número ingresado.
-'''
 
 num = int(input("Ingrese un número: "))
 while num % 3 != 0:
