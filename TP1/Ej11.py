9e5e16da5f0114a33ba124a9313f23dc0a2000ee
Matías Alexander Hillmann <mailmatiash@gmail.com>
2022-09-16 10:58:33 -0300

    Update Ej11.py

--- TP1/Ej11.py
+++ TP1/Ej11.py
@@ -1,12 +1,3 @@
-'''
-Se ingresa el código de tipo de llamada: 
-1. Local 
-2. Interurbana 
-3. Internacional 
-y la duración en minutos de la misma. Si el minuto cuesta $0.25 para la llamada local,
-$0.40 para la llamada interurbana y $1.05 para la llamada internacional, diseñar un
-algoritmo que permita calcular el monto a pagar por dicha llamada.
-'''
 
 precio_minuto_local			= float(0.25)
 precio_minuto_interurbano	= float(0.40)
