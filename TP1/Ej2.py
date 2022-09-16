64ed6d528f4fe4f3eee0b3d15ec8449a32328985
Matías Alexander Hillmann <mailmatiash@gmail.com>
2022-09-16 10:52:12 -0300

    Update Ej2.py

--- TP1/Ej2.py
+++ TP1/Ej2.py
@@ -1,5 +1,3 @@
-# Calcular el importe que debe pagar una persona compra una heladera de pesos X y
-# por pagar en efectivo le hacen el 10% de descuento ¿Cuánto abona?
 
 precio_heladera = float(input("Ingrese el precio de la heladera:\n"))
 
