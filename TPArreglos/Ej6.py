dc96b46203e0b44179913a5705eb86614c9fdcab
gitMatiH <mailmatiash@gmail.com>
2022-10-18 17:18:38 -0300

    mejora ej6

--- TPArreglos/Ej6.py
+++ TPArreglos/Ej6.py
@@ -14,28 +14,25 @@ print("La lista en cuestión:")
 print(lista)
 
 def intercambiar(i,j,lista):
+    #precondicion i<j
     #toma dos elementos de pos i y j de una lista y los intercambia
-    aux = lista[i]
-    lista[i] = lista[j]
-    lista[j] = aux
+    aux = lista[j]
+    lista[j] = lista[i]
+    lista[i] = aux
     return lista
 
-ranking = []    # queda en nosotros que la lista no se pase de los diez elementos
-# si la lista es de solo diez elementos,
-# van a quedar esos diez elementos en el ranking
-# y van a estar ordenados de mayor a menor
+ranking = []
+longitud = 10   # queremos un ranking de diez elem
+aux = lista[0]
+for i in range(0,longitud):
+    ranking.append(aux)
+
 while i < len(lista):
-    # repasa el ranking de menor a mayor, o sea del ultimo al primero
-    # y desplaza si es mayor, para y lo mete en la posicion actual
-    # cuando el siguiente es mayor
-    # para recorrer, vamos a usar j, j+1
-    # para designar actual y proximo
-    j = len(ranking)
-    if j==0: #lista vacia, alternativo ranking == False
-        ranking.append(lista[i])
-    while j <= 0:
-        if ranking[j-1] >= lista[i]:
+    j = len(ranking)-1
+    elem = lista[i]
+    while j > 0:
+        if ranking[j-1] < ranking[j]:
             lista = intercambiar(j-1,j,lista)
-        elif ranking[j-1] < lista[i]:
-            pass
         j = j - 1
+
+print(ranking)
