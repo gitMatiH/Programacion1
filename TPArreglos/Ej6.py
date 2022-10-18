4e0817b47af0a8c822f41043e27c00410c33321f
gitMatiH <mailmatiash@gmail.com>
2022-10-18 18:02:34 -0300

    finalmente anda el ej6

--- TPArreglos/Ej6.py
+++ TPArreglos/Ej6.py
@@ -13,26 +13,31 @@ print("Longitud de la lista generada y llenada en forma aleatoria: ", len(lista)
 print("La lista en cuestión:")
 print(lista)
 
-def intercambiar(i,j,lista):
-    #precondicion i<j
-    #toma dos elementos de pos i y j de una lista y los intercambia
-    aux = lista[j]
-    lista[j] = lista[i]
-    lista[i] = aux
-    return lista
+##def intercambiar(i,j,lista):
+##    #precondicion i<j
+##    #toma dos elementos de pos i y j de una lista y los intercambia
+##    aux = lista[j]
+##    lista[j] = lista[i]
+##    lista[i] = aux
+##    return lista
 
 ranking = []
 longitud = 10   # queremos un ranking de diez elem
-aux = lista[0]
 for i in range(0,longitud):
-    ranking.append(aux)
+    ranking.append(lista[i])
+print(ranking)
 
+i = 0
 while i < len(lista):
-    j = len(ranking)-1
+    pos = len(ranking)-1
     elem = lista[i]
-    while j > 0:
-        if ranking[j-1] < ranking[j]:
-            lista = intercambiar(j-1,j,lista)
-        j = j - 1
-
+    if ranking[pos] < elem:
+        ranking[pos] = elem
+        while pos > 0 and ranking[pos-1] < ranking[pos]:
+            #lista = intercambiar(j-1,j,lista) ¿por que esto no anda?
+            aux = ranking[pos-1]
+            ranking[pos-1] = ranking[pos]   #promociona
+            ranking[pos] = aux
+            pos = pos - 1
+    i = i + 1
 print(ranking)
