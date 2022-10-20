51f53e01c3ca9b33dc43a7d9a4df1c7b194a06c9
Matías Hillmann <mailmatiash@gmail.com>
2022-10-20 18:28:39 -0300

    Ej7

---
+++ TPArreglos/Ej7.py
@@ -0,0 +1,52 @@
+'''
+Cargar dos arreglos de enteros de N y M posiciones.
+Se pide generar un programa que produzca la intersección
+entre los dos arreglos.
+'''
+#que sera mas rapido, ordenar la lista primero y comparar después
+#o, si esta en interseccion, pasar y si no esta en interseccion,
+#comparar cada elemento de la primer lista con
+#todos los elementos de la segunda lista
+#... o dará lo mismo?
+
+import random
+
+#para test sin random
+#lista_a = [32, 94, 75, 31, 65, 38, 7, 79, 42, 70, 11, 57]
+lista_a = []
+n = random.randint(10, 20)
+for i in range(0,n):
+    lista_a.append(random.randint(1, 100))
+print("Lista de: ", len(lista_a),"elementos")
+print("La lista en cuestión:")
+lista_a.sort()
+print(lista_a)
+
+#para test sin random
+#lista_b = [32, 94, 44, 33, 65, 38, 7, 79, 42, 70, 11, 57]
+lista_b = []
+m = random.randint(10, 20)
+for j in range(0,m):
+    lista_b.append(random.randint(1, 100))
+print("Lista de: ", len(lista_b),"elementos")
+print("La lista en cuestión:")
+lista_b.sort()
+print(lista_b)
+
+interseccion = []
+
+i = 0
+j = 0
+while i!=len(lista_a) and j!=len(lista_b):
+    if lista_a[i] < lista_b[j]:
+        i = i+1
+    else:
+        if lista_a[i] > lista_b[j]:
+            j = j+1
+        else:   # son iguales
+            interseccion.append(lista_a[i]) #también lo podríamos haber hecho con lista_b[i] 
+            i = i+1
+            j = j+1
+
+print(interseccion)
+        
