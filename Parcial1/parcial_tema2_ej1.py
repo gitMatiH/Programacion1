834bc319d583d3b683c4956bfa0907b1e931be61
gitMatiH <mailmatiash@gmail.com>
2022-10-25 14:59:31 -0300

    enunciados parcial tema 2

---
+++ Parcial1/parcial_tema2_ej1.py
@@ -0,0 +1,14 @@
+'''
+Se quiere generar una estadística de búsquedas en Internet
+y para ello se requiere la siguiente información:
+Palabra clave
+Cantidad de búsquedas en Argentina
+Cantidad de búsquedas en el resto del mundo
+
+La carga de datos finaliza cuando se ingresa la palabra FIN
+en la palabra clave
+
+Se pide
+-Mostrar la palabra clave que tiene más búsquedas en la Argentina
+-Calcular y mostrar el promedio de búsquedas en el resto del Mundo
+'''
---
+++ Parcial1/parcial_tema2_ej2.py
@@ -0,0 +1,12 @@
+'''
+Se quiere procesar un lote de números que pueden ser
+enteros positivos o negativos.
+Se piden números hasta que se ingrese un número positivo
+divisible por 5
+Se pide:
+-Calcular el pordentaje de los números impares ingresados
+y mostrar la leyenda: "Menos del 20% de los números son impares"
+o "Se ingresaron más del 20% de impares"
+-Determinar y mostrar cuántos números ingresados se encuentran
+entre el -1 (inclusive) y 12 (inclusive).
+'''
