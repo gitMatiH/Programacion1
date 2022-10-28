0fc570c6d47d24533d42438e3ed22cbfb366bbae
gitMatiH <mailmatiash@gmail.com>
2022-10-28 08:20:14 -0300

    ejercicio parcial

--- Parcial1/parcial_tema2_ej1.py
+++ Parcial1/parcial_tema2_ej1.py
@@ -11,9 +11,35 @@ en la palabra clave
 Se pide
 -Mostrar la palabra clave que tiene más búsquedas en la Argentina
 -Calcular y mostrar el promedio de búsquedas en el resto del Mundo
-
-
-
-
-
 '''
+
+#Ejercicio 1
+acumulador_cant_busquedas_resto = 0
+contador = 0
+
+##Bloque de ingreso
+palabra_clave = input("Ingrese una palabra clave, 'FIN' para finalizar ingreso: ")
+####
+    
+mas_buscada_arg = palabra_clave
+cant_mas_buscada_arg = cant_busquedas_arg
+while palabra_clave != 'FIN':
+    
+    cant_busquedas_arg = int(input("Ingrese la cantidad de búsquedas para la palabra clave en argentina: "))
+    cant_busquedas_resto = int(input("Ingrese la cantidad de búsquedas para la palabra clave en el resto del mundo: "))
+    acumulador_cant_busquedas_resto = acumulador_cant_busquedas_resto + cant_busquedas_resto
+    contador = contador + 1
+    
+    if cant_busquedas_arg > cant_mas_buscada_arg:
+        mas_buscada_arg = palabra_clave
+        cant_mas_buscada_arg = cant_busquedas_arg
+    
+    ##Bloque de ingreso
+    palabra_clave = input("Ingrese una palabra clave, 'FIN' para finalizar ingreso: ")
+    ####
+
+if contador != 0:
+    promedio = acumulador_cant_busquedas_resto/contador
+
+print("La palabra clave que tiene más búsquedas en la argentina es: ", mas_buscada_arg)
+print("El promedio de búsquedas por palabra en el resto del mundo es: ", promedio)
