eb9306d8317dd8108c993610465e9882bad05b6c
gitMatiH <mailmatiash@gmail.com>
2022-11-04 12:26:45 -0300

    practica para parcial

---
+++ test.py
@@ -0,0 +1,12 @@
+arreglo = [5,4,3,2,1]
+promedio = 0
+acumulador = 0
+cantidad= len(arreglo)
+i=0
+print(arreglo)
+while i < cantidad:
+    #procesa
+    print("paso ", i)
+    print( "elemento actual: ", arreglo[i])
+    #actualiza cierta cosa
+    i=i+1
---
+++ tipoParcial_Ejercicio4.py
@@ -0,0 +1,51 @@
+'''
+Ingresar valores en un array.
+Solicitar la cantidad de elementos a ingresar.
+Luego ingresar la cantidad indicada por el usuario.
+Escribir una función que reciba como parámetro el array
+y dos posiciones. Si las posiciones son válidas intercambiar
+los elementos.
+
+Escribir una función que devuelva el promedio de los elementos
+del array.
+Escribir una función que devuelva todos los elementos menores
+que el promedio del array.
+'''
+
+def intercambiar(datos, pos1, pos2):
+    if (pos1>=0 and pos1<len(datos)) and (pos2>=0 and pos2<len(datos)):
+        aux=datos[pos1]
+        datos[pos1]=datos[pos2]
+        datos[pos2]=aux
+    else:
+        print("No son posiciones válidas.")
+
+def promedio(arreglo):
+    promedio = 0
+    acumulador = 0
+    cantidad= len(arreglo)
+    i=0
+    while (condicion):
+        #procesa
+
+        #actualiza cierta cosa
+        
+    return promedio
+
+def menoresAPromedio(promedio, array):
+    menores = []
+    return menores
+
+datos = []
+cantidad = int(input("Cuantos elementos: "))
+if cantidad>0:
+    for i in range(0, cantidad):
+        datos.append(int(input("Ingrese elemento: ")))
+
+print(datos)
+p1=int(input("indice primer num: "))
+p2=int(input("indice segundo num: "))
+intercambiar(datos, p1, p2)
+print(datos)
+
+prom = promedio(datos)
