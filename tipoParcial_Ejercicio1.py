d6c20721e222729362fc24fc631ee73475b3334e
Matías Hillmann <mailmatiash@gmail.com>
2022-11-10 07:55:31 -0300

    otro ejercicio tipo parcial

--- tipoParcial_Ejercicio1.py
+++ tipoParcial_Ejercicio1.py
@@ -24,3 +24,46 @@ numero 2 = 3
 
 arreglo = [4,-9,7,5,3,8]
 '''
+
+## interpreté que la función swap debe discernir si los números son válidos o no
+## alternativamente, se podría escribir una función validar() y usarla para cada
+## ingreso de los índices.
+
+def swap(a, b, arr):
+    if (a<0 or a>len(arr)) or (b<0 or b>len(arr)):
+        return -1
+    else:
+        if arr[a] != arr[b]:
+            aux = arr[a]
+            arr[a] = arr[b]
+            arr[b] = aux
+        elif arr[a] == arr[b]:
+            arr[a] = 0
+            arr[b] = 0
+        return 0
+
+
+N = int(input("Ingrese el tamaño del arreglo a llenar: "))
+
+i = 0
+arreglo = []
+while i < N:
+    elemento = int(input("Ingrese un número entero múltiplo de tres pero no múltiplo de 5: "))
+    if (elemento%3 != 0 or elemento%5 == 0):    # equivalente a not(notcond1 and notcond2) -> not(elemento%3 == 0 and elemento%5 != 0)
+        print("Número inválido.")
+        elemento = int(input("Ingrese un número entero múltiplo de tres pero no múltiplo de 5: "))
+    arreglo.append(elemento)
+    i = i +1
+
+print(arreglo)
+
+print("Ingrese dos índices: ")
+indice_a = int(input("Indice a: "))
+indice_b = int(input("Indice b: "))
+
+sal = swap(indice_a, indice_b, arreglo)
+if sal == 0:
+    print("La operación fue exitosa.")
+    print("El arreglo con cambios: ", arreglo)
+elif sal == -1:
+    print("Error, índices fuera de rango.")
