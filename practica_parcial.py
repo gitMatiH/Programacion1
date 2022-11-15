913a711a53e772e008660dc9c22d6b4a2fb6bf37
Matías Hillmann <mailmatiash@gmail.com>
2022-11-15 18:22:25 -0300

    agrego cosas

--- practica_parcial.py
+++ practica_parcial.py
@@ -25,6 +25,7 @@ def promedio_f(edades, generos):
         promedio = acumulador/contador
     return promedio
 
+'''
 def minimo_edad_m(edades, generos, nombres):
     ## no pide nombres, pero lo hacemos para practicar
     
@@ -50,14 +51,58 @@ def minimo_edad_m(edades, generos, nombres):
         return edad_min, nombre
     nombre = nombres[pos_min]
     return edad_min, nombre
-    
+'''
+
+def cant_edad_min_m(edades, generos, edad):
+    contador = 0
+    for i in range(0, len(edades)):
+        if edades[i] == edad and generos[i] == 'M':
+            contador = contador + 1
+    return contador
+
+def cant_edad_min_m_2(edades, generos):
+    minimo = minimo_edad_m(edades, generos)
+    contador = 0
+    for i in range(0, len(edades)):
+        if edades[i] == minimo and generos[i] == 'M':
+            contador = contador + 1
+    return contador
+
+def nombres_edad_min_m(nombres, edades, generos, edad_min):
+    nombres_min = []
+    for i in range(0, len(nombres)):
+        if edades[i] == edad_min and generos[i] == 'M':
+            nombres_min.append(nombres[i])
+    return nombres_min
+
+def minimo_edad_m(edades, generos):
+    ## no pide nombres, pero lo hacemos para practicar
+    i = 0
+    # vamos a recorrer la lista de géneros y vamos a buscar el primer masculino.
+    # como puede pasar que no haya ningún masculino, ponemos la condición "i<len(generos)"
+    while i<len(generos) and generos[i] != "M":
+        i=i+1
+
+    # vamos a enganchar el siguiente if con la condición de "i<len(generos)"
+    # la cual nos dice si hubo o no hubo participantes masculinos, y en base a eso procesa o no
+    if i<len(generos):
+        #procesar
+        edad_min = edades[i]   # inicializamos con el que cortó el while, que es el primer masculino que queríamos
+        pos_min = i
+        for j in range(i,len(generos)): #recorremos el resto del array
+            if edades[j]<edad_min and generos[j] == 'M':
+                edad_min = edades[j]
+    else:
+        edad_min = -1
+        return edad_min
+    return edad_min
 
 edades = []
 nombres = []
 generos = []
 categorias = []
 
-edades = [12, 56, 45, 22]
+edades = [12, 56, 12, 22]
 nombres = ['Juli', 'Mariana', 'Cacho', 'Maria']
 generos = ['M', 'F', 'M', 'F']
 categorias = ['MEN', 'MAY', 'MAY', 'MEN']
@@ -98,5 +143,21 @@ print(categorias)
 prom = promedio_f(edades, generos)
 print("El promedio de edades femenino es: ", prom)
 
-edad_min, nombre = minimo_edad_m(edades, generos, nombres)
-print("El participante masculino de menor edad es", nombre,"y tiene", edad_min, "años.")
+edad_min = minimo_edad_m(edades, generos)
+print("El participante masculino de menor edad tiene", edad_min, "años.")
+
+# hay dos maneras de hacerlo
+# método 1) llamar a la función minimo_edad_m afuera de nuestra función
+edad_m = minimo_edad_m(edades, generos)
+cantmin = cant_edad_min_m(edades, generos, edad_min)
+print("La cantidad de veces que apareció la mínima edad de participantes masculinos: ", cantmin)
+
+# método 2) llamar a la función minimo_edad_m dentro de la función que definimos
+cantmin = cant_edad_min_m_2(edades, generos)
+print("La cantidad de veces que apareció la mínima edad de participantes masculinos: ", cantmin)
+
+
+edad_m = minimo_edad_m(edades, generos)
+nombres_min = nombres_edad_min_m(nombres, edades, generos, edad_m)
+print("Los nombres de los participantes con la edad mínima:")
+print(nombres_min)
